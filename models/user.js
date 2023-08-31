d01a3f1d184bc52cb33cb65f2b82aefe69508269
axdevil <deb.devel@outlook.es>
2023-08-30 20:00:56 -0600

    User Schema

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        birthday: { type: Date, required: false },
+        name: { type: String, required: true },
+        lastname: { type: String, required: true },
+        preferences: [String],
+        picture: Buffer,
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+module.exports = model("User", userSchema);
