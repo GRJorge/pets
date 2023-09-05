5e5081f76aa8b4e52dfcdcca49f8182113d2a393
axdevil <deb.devel@outlook.es>
2023-09-05 09:41:42 -0600

    Muestra de errores

--- public/javascripts/validateForms.js
+++ public/javascripts/validateForms.js
@@ -1,7 +1,15 @@
+const errorsContainer = document.querySelector('#errors')
+
 document.querySelectorAll("form").forEach((form) => {
 	form.addEventListener("submit", (event) => {
+		//BORRAR TODOS LOS ERRORES
+		errorsContainer.innerHTML = ""
 		// MINIMO DE CARACTERES EN GENERAL
 		form.querySelectorAll("input").forEach((text) => {
+			if(text.classList.contains('textAlert')){ //QUITAR INPUTS DE ALERTA
+				text.classList.replace('textAlert','textPrimary')
+			}
+			//
 			if (text.value.length < text.getAttribute("maxL")) {
 				inputError(text, " debe contener minimo " + text.getAttribute("maxL") + " caracteres", event);
 			}
@@ -23,6 +31,23 @@ document.querySelectorAll("form").forEach((form) => {
 });
 
 function inputError(input, msg, event) {
-	alert(input.getAttribute("placeholder") + msg);
-	event.preventDefault();
+	event.preventDefault() //PARAR ENVIO DE FORMULARIO
+	changeClassToAlert(input) //CAMBIAR INPUTS A ALERT
+	const tag = '<span class="notError">' + input.getAttribute('placeholder') + msg + '<button id="close"><i class="fi fi-sr-circle-xmark"></i></button></span>'
+	errorsContainer.innerHTML += tag //AGREGAR ERROR
+	errorsContainer.style.display = "flex" //MOSTRAR CONTENEDOR DE ERRORES
+	errorsContainer.querySelectorAll('#close').forEach(close => { //QUITAR ERRORES AL DAR CLICK EN CERRAR
+		close.addEventListener('click',()=>{
+			close.parentNode.remove()
+			if(errorsContainer.childElementCount == 0){
+				errorsContainer.style.display = "none" //ESCONDER CONTENEDOR CUANDO NO HAYA ERRORES
+			}
+		})
+	})
+}
+
+function changeClassToAlert(input){ //REEMPLAZAR CLASES
+	if(input.classList.contains('textPrimary')){
+		input.classList.replace('textPrimary', 'textAlert')
+	}
 }
