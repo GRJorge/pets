51a42917dfb4f00e589e25c6785729ea73d037c1
axdevil <deb.devel@outlook.es>
2023-09-15 17:57:53 -0600

    Preview de fotos en nueva publicacion

--- public/javascripts/main/index.js
+++ public/javascripts/main/index.js
@@ -1,8 +1,43 @@
 const description = document.querySelector("#description");
 const postSubmit = document.querySelector("#postSubmit");
+const multimediaButton = document.querySelector("#multimedia");
+const gallery = document.querySelector("#gallery");
 
 //TEXTAREA AUTORESIZABLE
 description.addEventListener("input", () => {
+    descriptionAutoresizable()
+});
+//OCULTAR ACCIONES DE PUBLICACION AL DAR RESET
+postSubmit.querySelector("button").addEventListener("click", () => {
+    description.value = " "
+    descriptionAutoresizable()
+    postSubmit.style.display = "none";
+    gallery.style.display = "none"
+    gallery.innerHTML = ""
+});
+//AGREGAR FOTO AL PREVIEW
+multimediaButton.addEventListener("change", () => {
+    gallery.style.display = "flex"
+    gallery.innerHTML = "";
+
+    for (let i = 0; i < multimediaButton.files.length; i++) {
+        const file = multimediaButton.files[i];
+        const img = document.createElement("img");
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            img.src = e.target.result;
+        };
+
+        reader.readAsDataURL(file);
+
+        gallery.appendChild(img);
+        postSubmit.style.display = "flex";
+    }
+});
+
+//FUNCION QUE CAMBIA EL TAMAÑO DEL TEXTAREA
+function descriptionAutoresizable(){
     description.style.height = "auto";
     description.style.height = description.scrollHeight - 16 + "px";
 
@@ -11,8 +46,4 @@ description.addEventListener("input", () => {
     } else {
         postSubmit.style.display = "none";
     }
-});
-//OCULTAR ACCIONES DE PUBLICACION AL DAR RESET
-postSubmit.querySelector("button").addEventListener("click", () => {
-    postSubmit.style.display = "none";
-});
+}
