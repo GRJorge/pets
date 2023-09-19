8834a4afad0af9108c8d28152b4501ffe50dfda2
axdevil <deb.devel@outlook.es>
2023-09-18 20:18:22 -0600

    Muestra de "mis publicaciones recientes"

--- controllers/main.js
+++ controllers/main.js
@@ -1,13 +1,29 @@
 const global = require("../config/global");
 const User = require("../models/user");
+const Publication = require("../models/publication");
 require("../config/db");
 
 module.exports = {
     viewHome: function (req, res) {
         global.ifSession(req, res, async () => {
-            const query = await User.findById(req.session.user).select("picture").lean()
-            
-            res.render("main/index",{ pictureProfile: query.picture });
+            const date = new Date();
+
+            //FOTO DE PERFIL
+            const pictureProfile = await User.findById(req.session.user)
+                .select("picture")
+                .lean();
+            //ULTIMAS PUBLICACIONES
+            const lastPublications = await Publication.find({
+                user: req.session.user,
+                createdAt: {
+                    $gte: date.setDate(date.getDate() - 3),
+                },
+            }).sort({ createdAt: -1 }).lean();
+
+            res.render("main/index", {
+                pictureProfile: pictureProfile.picture,
+                lastPublications
+            });
         });
     },
 };
