25ea3f10b24acefdb1cda649d2151d425e6416bd
axdevil <deb.devel@outlook.es>
2023-09-20 10:35:40 -0600

    Validacion de nueva publicacion

--- controllers/publication.js
+++ controllers/publication.js
@@ -2,19 +2,34 @@ const Publication = require("../models/publication");
 require("../config/db");
 
 module.exports = {
-    new: async function (req, res) {
-        let files = [];
+    new: function (req, res) {
+        const errors = [];
+        console.log(req.files.length);
 
-        req.files.forEach((file) => {
-            files.push(file.filename);
-        });
+        if (req.body.description.replace(/\s/g,"") === ""){ //COMPRUEBA SI EL DESCRIPTION ESTE VACIO
+            if (req.files.length > 0) { //COMPRUEBA SI SE SUBIERON FOTOS
+                savePublication();
+            }else{
+                res.redirect("/")
+            }
+        }else{
+            savePublication();
+        }
 
-        await new Publication({
-            user: req.session.user,
-            description: req.body.description,
-            multimedia: files,
-        }).save();
+        async function savePublication() {
+            let files = [];
 
-        res.redirect("/");
+            req.files.forEach((file) => {
+                files.push(file.filename);
+            });
+
+            await new Publication({
+                user: req.session.user,
+                description: req.body.description,
+                multimedia: files,
+            }).save();
+
+            res.redirect("/");
+        }
     },
 };
