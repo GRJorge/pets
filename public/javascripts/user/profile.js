1e66cffb0e804d7a484539280051cfb3c3723b93
axdevil <deb.devel@outlook.es>
2023-10-02 20:18:34 -0600

    Cambio de secciones y section de galeria

---
+++ public/javascripts/user/profile.js
@@ -0,0 +1,22 @@
+document.querySelector("#sections").querySelectorAll("button").forEach((button, btnIndex) => {
+    button.addEventListener("click",() => {
+        document.querySelectorAll("#section").forEach((section, scIndex) => {
+            if(btnIndex == scIndex){
+                section.style.display = "flex"
+                resetButtons(btnIndex)
+            }else{
+                section.style.display = "none"
+            }
+        })
+    })
+})
+
+function resetButtons(btnIndex){
+    document.querySelector("#sections").querySelectorAll("button").forEach((button, index) => {
+        if(index == btnIndex){
+            button.classList.replace("btnLight2","btnPrimary")
+        }else{
+            button.classList.replace("btnPrimary","btnLight2")
+        }
+    })
+}
