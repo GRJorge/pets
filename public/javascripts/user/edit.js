64be94c5897c943d59c84b8c1c778c8395eb47b5
axdevil <deb.devel@outlook.es>
2023-10-13 11:39:43 -0600

    Formulario y logica para cambio de foto de perfil

--- public/javascripts/user/edit.js
+++ public/javascripts/user/edit.js
@@ -2,6 +2,41 @@ const d = document;
 
 const errors = d.querySelector("#errors");
 
+/*---- EDICION DE FOTO DE PERFIL ----*/
+
+const picture = document.querySelector("#picture");
+const picturePreview = document.querySelector("#picturePreview");
+const oldSrcPicture = picturePreview.src;
+
+picture.addEventListener("change", () => {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+        picturePreview.src = e.target.result;
+    };
+
+    reader.readAsDataURL(picture.files[0]);
+
+    showControlsImage(true);
+});
+
+d.querySelector("#cancelEditPicture").addEventListener("click", () => {
+    showControlsImage(false);
+});
+
+function showControlsImage(show) {
+    if (show) {
+        d.querySelector("#controlsImage").style.display = "flex";
+        picture.labels[1].style.display = "none";
+    } else {
+        d.querySelector("#controlsImage").style.display = "none";
+        picture.labels[1].style.display = "block";
+        picturePreview.src = oldSrcPicture;
+    }
+}
+
+/*---- EDICION DE NOMBRE ----*/
+
 const editName = d.querySelector("#editName");
 const showEditNameBtn = d.querySelector("#showEditName");
 //CUANDO SE PRESIONA BOTON DE EDITAR NOMBRE
